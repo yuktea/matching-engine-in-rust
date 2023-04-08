568f61773c63e62f9eeeee82c0f9f155876c6395
yuktea <73362292+yuktea@users.noreply.github.com>
2023-04-08 05:40:26 +0530

    init order and order book

--- main.rs
+++ main.rs
@@ -1,8 +1,36 @@
 #[derive(Debug)]
+struct OrderBook {
+    asks: HashMap<Price, Limit>,
+    bids: HashMap<Price, Limit>,
+}
+
+impl OrderBook {
+    fn new() -> OrderBook {
+        OrderBook {
+            asks: HashMap::new(),
+            bids: HashMap::new(),
+        }
+    }
+
+    fn add_order(&mut self, order: Order) {
+        match order.bid_ask {
+            BidOrAsk::Ask => {
+                let price = Price::from_price(order
+            }
+            BidOrAsk::Bid => {
+                self.bids.insert(order.price, order);
+            }
+        }
+    }
+}
+
+
+#[derive(Debug)]
 enum BidOrAsk {
     Bid,
     Ask,
 }
+#[derive(Debug)]
 struct Price {
     integral: u64,
     fractional : u64,
@@ -22,6 +50,48 @@ impl Price {
     }
 }
 
+#[derive(Debug)]
+struct Limit {
+    price: Price,
+    order: Vec<Order>,
+}
+
+impl Limit {
+    fn new(price: f64) -> Limit {
+        Limit {
+            price: Price::new(price),
+            order: Vec::new(),
+        }
+    }
+
+    fn add_order(&mut self, order: Order) {
+        self.order.push(order);
+    }
+}
+
+#[derive(Debug)]
+struct Order {
+    size: f64,
+    bid_ask: BidOrAsk
+}
+
+impl Order {
+    fn new(bid_ask: BidOrAsk, size: f64) -> Order {
+        Order {
+                    bid_ask,
+                    size,
+                }
+    }
+}
+
 fn main() {
-    println!("first commit");
+    let mut limit = Limit::new(65.3);
+    let price = Price::new(90.5);
+    let buy = Order::new(BidOrAsk::Bid, 5.1);
+    let sell = Order::new(BidOrAsk::Ask, 2.4);
+
+    limit.add_order(buy);
+    limit.add_order(sell);
+
+    println!("{:?}", limit); 
 }
