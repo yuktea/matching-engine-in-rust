fb1d76e9e14a7f5e0f3ca8109156153168706728
yuktea <73362292+yuktea@users.noreply.github.com>
2023-04-08 05:39:19 +0530

    add price implementation

--- main.rs
+++ main.rs
@@ -3,6 +3,25 @@ enum BidOrAsk {
     Bid,
     Ask,
 }
+struct Price {
+    integral: u64,
+    fractional : u64,
+    scalar : u64,
+}
+
+impl Price {
+    fn new(price: f64) -> Price {
+        let scalar = 100000;
+        let integral = price as u64;
+        let fractional = ((price % 1.0) * scalar as f64) as u64;
+        Price {
+            integral,
+            scalar,
+            fractional,
+        }
+    }
+}
+
 fn main() {
     println!("first commit");
 }
