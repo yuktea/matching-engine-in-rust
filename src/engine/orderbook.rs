0f89517f971a573c03074316953657df24dd667e
yuktea <73362292+yuktea@users.noreply.github.com>
2023-05-10 03:05:40 +0530

    refactor: add orderbook class

---
+++ src/engine/orderbook.rs
@@ -0,0 +1,102 @@
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub struct OrderBook {
+    asks: HashMap<Price, Limit>,
+    bids: HashMap<Price, Limit>,
+}
+
+pub impl OrderBook {
+    pub fn new() -> OrderBook {
+        OrderBook {
+            asks: HashMap::new(),
+            bids: HashMap::new(),
+        }
+    }
+
+    pub fn add_order(&mut self, price: f64, order: Order) {
+        let price = Price::new(price);
+
+        match order.bid_ask {
+            BidOrAsk::Bid =>
+
+                match self.bids.get_mut(&price) {
+                    Some(limit) => limit.add_order(order),
+                    None => {
+                        let mut limit = Limit::new(price);
+                        limit.add_order(order);
+                        self.bids.insert(price, limit);
+                    }
+                },
+                BidOrAsk::Ask => match self.asks.get_mut(&price) {
+                    Some(limit) => limit.add_order(order),
+                    None => {
+                        let mut limit = Limit::new(price);
+                        limit.add_order(order);
+                        self.asks.insert(price, limit);
+                    }
+
+            },
+        }
+    }
+}
+
+
+#[derive(Debug)]
+enum BidOrAsk {
+    Bid,
+    Ask,
+}
+#[derive(Debug, Eq, PartialEq, Hash, Clone, Copy)]
+pub struct Price {
+    integral: u64,
+    fractional : u64,
+    scalar : u64,
+}
+
+impl Price {
+    pub fn new(price: f64) -> Price {
+        let scalar = 100000;
+        let integral = price as u64;
+        let fractional = ((price % 1.0) * scalar as f64) as u64;
+        Price {
+            integral,
+            scalar,
+            fractional,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Limit {
+    price: Price,
+    order: Vec<Order>,
+}
+
+impl Limit {
+    pub fn new(price: Price) -> Limit {
+        Limit {
+            price,
+            order: Vec::new(),
+        }
+    }
+
+    fn add_order(&mut self, order: Order) {
+        self.order.push(order);
+    }
+}
+
+#[derive(Debug)]
+pub struct Order {
+    size: f64,
+    bid_ask: BidOrAsk
+}
+
+impl Order {
+    pub fn new(bid_ask: BidOrAsk, size: f64) -> Order {
+        Order {
+                    bid_ask,
+                    size,
+                }
+    }
+}
