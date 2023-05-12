6f743191ca6384648eacbda8fed5fe0f794f52db
yuktea <73362292+yuktea@users.noreply.github.com>
2023-05-12 12:13:26 +0530

    add trading pairs

--- src/engine/matching_engine.rs
+++ src/engine/matching_engine.rs
@@ -1,3 +1,22 @@
+use super::orderbook::{Order, Orderbook};
+use std::collections::HashMap;
+
+#[derive(Debug, Eq, PartialEq, Hash, Clone)]
+pub struct TradingPair {
+    base: String,
+    quote: String,
+}
+
+impl TradingPair {
+    pub fn new(base: String, quote: String) -> TradingPair {
+        TradingPair { base, quote }
+    }
+
+    pub fn to_string(self) -> String {
+        format!("{}_{}", self.base, self.quote)
+    }
+}
+
 pub struct MatchingEngine {
     orderbooks: HashMap<TradingPair, Orderbook>,
 }
