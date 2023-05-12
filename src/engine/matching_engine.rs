97f5fcbe51d8d5507b3572e4ad5b0649512142ad
yuktea <73362292+yuktea@users.noreply.github.com>
2023-05-12 12:09:35 +0530

    implement matching engine

--- src/engine/matching_engine.rs
+++ src/engine/matching_engine.rs
@@ -1,3 +1,11 @@
 pub struct MatchingEngine {
     orderbooks: HashMap<TradingPair, Orderbook>,
 }
+
+impl MatchingEngine {
+    pub fn new() -> MatchingEngine {
+        MatchingEngine {
+            orderbooks: HashMap::new(),
+        }
+    }
+}
