aa5de94387beb27987257344b1b0ad85587b676f
yuktea <73362292+yuktea@users.noreply.github.com>
2023-09-26 03:38:37 +0530

    Revert "test"
    This reverts commit 2b4211b908557deef8e37aa3b538fa24e0eb4fd5.

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,21 @@
 mod engine;
 
-use engine::engine::{MatchingEngine, TradingPair};
-use engine::orderbook::{BidOrAsk, Order, Orderbook};
+use engine::orderbook::{Order, BidOrAsk, Orderbook};;
 
 fn main() {
-    let buy_order_from_alice = Order::new(BidOrAsk::Bid, 5.5);
-    let buy_order_from_bob = Order::new(BidOrAsk::Bid, 2.45);
+    let bid = Order::new(BidOrAsk::Bid, 5.0);
+    let ask = Order::new(BidOrAsk::Ask, 2.0);
 
-    let mut orderbook = Orderbook::new();
-    orderbook.add_order(4.4, buy_order_from_alice);
-    orderbook.add_order(4.4, buy_order_from_bob);
 
-    let sell_order = Order::new(BidOrAsk::Ask, 6.5);
-    orderbook.add_order(20.0, sell_order);
 
-    // debug stm println!("{:?}", orderbook);
 
-    let mut engine = MatchingEngine::new();
-    let pair = TradingPair::new("BTC".to_string(), "USD".to_string());
-    engine.add_new_market(pair.clone());
+    let mut orderbook = OrderBook::new();
+    orderbook.add_order(2.0, bid);
+    orderbook.add_order(4.0, ask);
 
-    let buy_order = Order::new(BidOrAsk::Bid, 6.5);
-    engine.place_limit_order(pair, 10.000, buy_order).unwrap();
+    let selling = Order::new(BidOrAsk::Ask, 6.5);
+    orderbook.add_order(20.0, selling);
+    println!("{:?}", orderbook); 
+
+
 }
